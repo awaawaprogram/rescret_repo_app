aa0c9febd5caa86bb49c4febf4643f5727c8b32a
awaawaprogram <awaawaprogram@gmail.com>
2023-04-06 00:12:22 +0900

    update line_gpt_lambda.py chatgptに変更してもらっている途中のプログラム。動かない可能性大。

--- line_gpt_lambda.py
+++ line_gpt_lambda.py
@@ -6,6 +6,8 @@ import requests
 import boto3
 import os
 import time
+import stripe
+
 
 LINE_CHANNEL_SECRET = os.getenv('LINE_CHANNEL_SECRET')
 LINE_CHANNEL_ACCESS_TOKEN = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
@@ -13,9 +15,16 @@ OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 OPENAI_COMPLETIONS_ENDPOINT = os.getenv('OPENAI_COMPLETIONS_ENDPOINT')
 LINE_REPLY_ENDPOINT = os.getenv('LINE_REPLY_ENDPOINT')
 
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
+PLAN_580_JPY = os.getenv('PLAN_580_JPY')
+PLAN_1080_JPY = os.getenv('PLAN_1080_JPY')
+
+USER_DATA_TABLE = 'user_data'
+CONVERSATION_HISTORY_TABLE = 'chat_history'
+
+
 dynamo = boto3.client('dynamodb', region_name='ap-southeast-2')
 
-TABLE_NAME='chat_history'
 
 def validate_type(event):
     print('validate_type')
@@ -39,7 +48,7 @@ def populate_conversation(user_id, message):
     print('generate_query - start')
     try:
         history = dynamo.get_item(
-            TableName=TABLE_NAME,
+            TableName=CONVERSATION_HISTORY_TABLE,
             Key={
                 'user_id': {
                     'S': user_id,
@@ -88,7 +97,7 @@ def store_conversation(user_id, query, openai_response):
     print('store_conversation')
     try:
         dynamo.put_item(
-        TableName=TABLE_NAME,
+        TableName=CONVERSATION_HISTORY_TABLE,
         Item={
             'user_id': {
                 'S': user_id,
@@ -137,7 +146,7 @@ def line_reply(reply_token, response, cost_jpy):
 def archive_conversation(user_id):
     try:
         history = dynamo.get_item(
-            TableName=TABLE_NAME,
+            TableName=CONVERSATION_HISTORY_TABLE,
             Key={
                 'user_id': {
                     'S': user_id,
@@ -146,7 +155,7 @@ def archive_conversation(user_id):
         ).get('Item').get('conversation').get('S')
 
         dynamo.put_item(
-            TableName=TABLE_NAME,
+            TableName=CONVERSATION_HISTORY_TABLE,
             Item={
                 'user_id': {
                     'S': user_id + "-" + str(int(time.time())),
@@ -160,13 +169,119 @@ def archive_conversation(user_id):
         print('failed to archive conversation: {}'.format(e))
 
     dynamo.delete_item(
-            TableName=TABLE_NAME,
+            TableName=CONVERSATION_HISTORY_TABLE,
             Key={
                 'user_id': {
                     'S': user_id,
                 }
             }
         )
+
+def send_upgrade_link(reply_token):
+    headers = {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer {}'.format(LINE_CHANNEL_ACCESS_TOKEN)
+    }
+    data = {
+        "replyToken": reply_token,
+        "messages": [
+            {
+                "type": "template",
+                "altText": "Upgrade to a paid plan",
+                "template": {
+                    "type": "buttons",
+                    "text": "料金プランにアップグレードしましょう",
+                    "actions": [
+                        {
+                            "type": "uri",
+                            "label": "580円プラン",
+                            "uri": "https://your_website.com/upgrade?plan_id={}".format(PLAN_580_JPY)
+                        },
+                        {
+                            "type": "uri",
+                            "label": "1080円プラン",
+                            "uri": "https://your_website.com/upgrade?plan_id={}".format(PLAN_1080_JPY)
+                        }
+                    ]
+                }
+            }
+        ]
+    }
+    r = requests.post(LINE_REPLY_ENDPOINT, headers=headers, data=json.dumps(data))
+    print('LINE response: {}'.format(r.json()))
+
+
+def is_paid_user(user_id):
+    try:
+        user_data = dynamo.get_item(
+            TableName=USER_DATA_TABLE,
+            Key={
+                'user_id': {
+                    'S': user_id,
+                }
+            }
+        ).get('Item')
+        return 'plan_id' in user_data
+    except:
+        return False
+
+
+def can_use_more_tokens(user_id, cost_jpy):
+    try:
+        user_data = dynamo.get_item(
+            TableName=USER_DATA_TABLE,
+            Key={
+                'user_id': {
+                    'S': user_id,
+                }
+            }
+        ).get('Item')
+        if 'tokens_used' not in user_data:
+            return True
+
+        tokens_used = float(user_data['tokens_used']['N'])
+        plan_id = user_data.get('plan_id', {}).get('S', '')
+
+        if plan_id == PLAN_580_JPY:
+            return tokens_used + cost_jpy <= 1000000
+        elif plan_id == PLAN_1080_JPY:
+            return True
+        else:
+            return tokens_used + cost_jpy <= 5
+    except:
+        return False
+
+def update_tokens_used(user_id, cost_jpy):
+    dynamo.update_item(
+        TableName=USER_DATA_TABLE,
+        Key={
+            'user_id': {
+                'S': user_id,
+            }
+        },
+        UpdateExpression="SET tokens_used = tokens_used + :t",
+        ExpressionAttributeValues={
+            ':t': {
+                'N': str(cost_jpy)
+            }
+        }
+    )
+
+def update_message_count(user_id):
+    dynamo.update_item(
+        TableName=USER_DATA_TABLE,
+        Key={
+            'user_id': {
+                'S': user_id,
+            }
+        },
+        UpdateExpression="ADD message_count :mc",
+        ExpressionAttributeValues={
+            ':mc': {
+                'N': '1'
+            }
+        }
+    )
 
 def lambda_handler(event, context):
     print(event)
@@ -190,19 +305,45 @@ def lambda_handler(event, context):
         return {
             'statusCode': 200
         }
+    
+    user_data = dynamo.get_item(
+        TableName=USER_DATA_TABLE,
+        Key={
+            'user_id': {
+                'S': user_id,
+            }
+        }
+    ).get('Item')
+    message_count = int(user_data.get('message_count', {}).get('N', '0'))
+
+    
+    if message_count > 5 and not is_paid_user(user_id):
+        send_upgrade_link(reply_token)
+        return {
+            'statusCode': 200
+        }
 
     try:
-        query = populate_conversation(user_id, message)
-        openai_response = openai_completions(query)
-        response = format_openai_response(openai_response)
-        cost_jpy = get_openai_cost_jpy(openai_response)
-        store_conversation(user_id, query, openai_response)
+        if not can_use_more_tokens(user_id, cost_jpy):
+            response = 'トークンの上限に達しました。プランをアップグレードしてください。'
+        else:
+            query = populate_conversation(user_id, message)
+            openai_response = openai_completions(query)
+            response = format_openai_response(openai_response)
+            cost_jpy = get_openai_cost_jpy(openai_response)
+            store_conversation(user_id, query, openai_response)
+            update_tokens_used(user_id, cost_jpy)
+            update_message_count(user_id)
+
     except:
         cost_jpy = 0
         response = 'OpenAIが壊れてました😢'
+
     
     line_reply(reply_token, response, cost_jpy)
 
     return {
         'statusCode': 200,
     }
+
+
